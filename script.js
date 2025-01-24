6e23700ef5271491c0d5e51204a773dc20ac5a26
Jonyboylovespie <jonathan.frawley@outlook.com>
2025-01-24 13:56:07 -0500

    push

--- script.js
+++ script.js
@@ -1,31 +1,10 @@
-﻿function solveWordle(url) {
-    let startIndex = url.indexOf("word=") + 5;
-    let endIndex = url.indexOf("&");
-    
-    let scrambledString = url;
-    if (startIndex > 4)
-    {
-        scrambledString = url.substring(startIndex);
-    }
-    if (endIndex > startIndex) 
-    {
-        scrambledString = url.substring(startIndex, endIndex);
-    }
-    
-    document.getElementById('answer').innerHTML = decrypt(scrambledString).toUpperCase();
-}
+﻿function findWeaknesses(name) {
 
-function decrypt(ciphertext) {
-    let keyword = "wordle";
-    let keywordLength = keyword.length;
-    let decryptedText = "";
+    let weaknesses = ['Weakness 1', 'Weakness 2', 'Weakness 3', 'Weakness 4', 'Weakness 5'];
 
-    for (let i = 0; i < ciphertext.length; i++) {
-        let char = ciphertext[i];
-        let keywordShift = keyword.charCodeAt(i % keywordLength) - 'a'.charCodeAt(0);
-        let decryptedChar = String.fromCharCode(((char.charCodeAt(0) - 'a'.charCodeAt(0) - keywordShift + 26) % 26) + 'a'.charCodeAt(0));
-        decryptedText += decryptedChar;
-    }
-
-    return decryptedText;
+    weaknesses.forEach(weakness => {
+        const listItem = document.createElement('li');
+        listItem.textContent = weakness;
+        document.getElementById('weaknesses').appendChild(listItem);
+    })
 }
