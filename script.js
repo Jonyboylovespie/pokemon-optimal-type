09b0986f26a7ee5d5f818b98f53340f73e128dcd
Jonyboylovespie <jonathan.frawley@outlook.com>
2025-01-24 20:24:43 -0500

    push

--- script.js
+++ script.js
@@ -1,10 +1,147 @@
-﻿function findWeaknesses(name) {
+﻿const colors = {
+    NORMAL: '#A8A77A',
+    FIRE: '#EE8130',
+    WATER: '#6390F0',
+    ELECTRIC: '#F7D02C',
+    GRASS: '#7AC74C',
+    ICE: '#96D9D6',
+    FIGHTING: '#C22E28',
+    POISON: '#A33EA1',
+    GROUND: '#E2BF65',
+    FLYING: '#A98FF3',
+    PSYCHIC: '#F95587',
+    BUG: '#A6B91A',
+    ROCK: '#B6A136',
+    GHOST: '#735797',
+    DRAGON: '#6F35FC',
+    DARK: '#705746',
+    STEEL: '#B7B7CE',
+    FAIRY: '#D685AD',
+};
 
-    let weaknesses = ['Weakness 1', 'Weakness 2', 'Weakness 3', 'Weakness 4', 'Weakness 5'];
+async function findWeaknesses(name) {
+    
+    let listItems = ["WEAKNESSES:"];
+    for (let attackType of await goodAttackTypes(name))
+    {
+        listItems.push(attackType.toUpperCase());
+    }
+    if (listItems.length === 1)
+    {
+        listItems = ["Name Invalid"];
+    }
+    else
+    {
+        listItems.push("RESISTANCES:");
+        for (let attackType of await badAttackTypes(name))
+        {
+            listItems.push(attackType.toUpperCase());
+        }
+    }
+    
+    let element = document.getElementById('weaknesses');
 
-    weaknesses.forEach(weakness => {
-        const listItem = document.createElement('li');
-        listItem.textContent = weakness;
-        document.getElementById('weaknesses').appendChild(listItem);
+    while (element.firstChild) 
+    {
+        element.removeChild(element.firstChild);
+    }
+
+    listItems.forEach(listItem => 
+    {
+        const listElement = document.createElement('li');
+        listElement.textContent = listItem;
+        if (listItem === "WEAKNESSES:" || listItem === "RESISTANCES:" || listItem === "Name Invalid")
+        {
+            listElement.style.fontSize = "30px";
+            listElement.style.color = "white";
+        }
+        else
+        {
+            listElement.style.color = colors[listItem];
+        }
+        element.appendChild(listElement);
     })
+}
+
+async function fetchFromPokeAPI(endpoint) 
+{
+    const baseUrl = 'https://pokeapi.co/api/v2/';
+    try 
+    {
+        const response = await fetch(`${baseUrl}${endpoint}`);
+
+        if (!response.ok) 
+        {
+            throw new Error(`Error: ${response.status} - ${response.statusText}`);
+        }
+
+        const data = await response.json();
+        return data;
+
+    } catch (error) 
+    {
+        console.error(`There was a problem fetching data from endpoint "${endpoint}":`, error);
+        return null;
+    }
+}
+
+async function getPokemonTypes(pokemonName) 
+{
+    let endpoint = "pokemon/" + pokemonName;
+    let pokemon = await fetchFromPokeAPI(endpoint);
+
+    if (pokemon && pokemon.types) 
+    {
+        return pokemon.types.map(typeInfo => typeInfo.type.name);
+    }
+
+    return [];
+}
+
+async function goodAttackTypes(pokemonName)
+{
+    let pokemonTypes = await getPokemonTypes(pokemonName);
+    let goodAttackList = [];
+    for (let pokemonType of pokemonTypes)
+    {
+        let endpoint = "type/" + pokemonType;
+        let type = await fetchFromPokeAPI(endpoint);
+        if (type && type.damage_relations && type.damage_relations.double_damage_from) 
+        {
+            let attackTypes = type.damage_relations.double_damage_from.map(double_damage_from_info => double_damage_from_info.name);
+            for (let attackType of attackTypes) 
+            {
+                goodAttackList.push(attackType);
+            }
+        }
+    }
+    return goodAttackList;
+}
+
+async function badAttackTypes(pokemonName)
+{
+    let pokemonTypes = await getPokemonTypes(pokemonName);
+    let goodAttackList = [];
+    for (let pokemonType of pokemonTypes) 
+    {
+        let endpoint = "type/" + pokemonType;
+        let type = await fetchFromPokeAPI(endpoint);
+        if (type && type.damage_relations && type.damage_relations.no_damage_from)
+        {
+            let attackTypes = type.damage_relations.no_damage_from.map(double_damage_from_info => double_damage_from_info.name);
+            for (let attackType of attackTypes)
+            {
+                goodAttackList.push(attackType);
+            }
+        }
+        if (type && type.damage_relations && type.damage_relations.half_damage_from)
+        {
+            let attackTypes = type.damage_relations.half_damage_from.map(double_damage_from_info => double_damage_from_info.name);
+            for (let attackType of attackTypes)
+            {
+                goodAttackList.push(attackType);
+            }
+        }
+    }
+    return goodAttackList;
 }
